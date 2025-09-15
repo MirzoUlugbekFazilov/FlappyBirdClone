3cc084fb0e7dddf10a5c22c3f88b827db7084ee9
Mirzo-Ulugbek Fazilov <mirzoulugbekfazilov@gmail.com>
2025-09-16 03:01:27 +0500

    app.js

--- app.js
+++ app.js
@@ -38,6 +38,7 @@ let isGameStarted = false;
 let messageToPlayer;
 let distanceCounter;
 let levelIndicator;
+let restartButton;
 
 // let num = [80, -80, 0][Math.floor(Math.random() * 3)];
 
@@ -49,8 +50,8 @@ function create() {
   let topColumns = this.physics.add.staticGroup();
   for (let i = 0; i < 5000; i++) {
     topColumns.create(
-      200 + 400 * i,
-      [60, 40, 0, -50, -100][Math.floor(Math.random() * 5)],
+      250 + 400 * i,
+      [50, 40, 0, -50, -90][Math.floor(Math.random() * 5)],
       "column"
     );
   }
@@ -65,14 +66,13 @@ function create() {
 
   for (let i = 0; i < 5000; i++) {
     topColumns.create(
-      400 + 400 * i,
+      450 + 400 * i,
       [450, 500, 400, 550][Math.floor(Math.random() * 4)],
       "column"
     );
   }
   const bro = this.physics.add.staticGroup();
-  const road = bro.create(2200, 833, "road").setScale(10).refreshBody();
-
+  const road = bro.create(9300, 1492, "road").setScale(30).refreshBody();
   bird = this.physics.add.sprite(30, 90, "bird").setScale(2.5);
   bird.setBounce(0.4);
   bird.setCollideWorldBounds(true);
@@ -87,6 +87,32 @@ function create() {
 
   cursors = this.input.keyboard.createCursorKeys();
   cursors.r = this.input.keyboard.addKey("r");
+  cursors.pointer = this.input.activePointer;
+
+  restartButton = this.add
+    .text(450, 250, "Restart", {
+      fontFamily: "Georgia, 'Times New Roman', Times, serif",
+      fontSize: "30px",
+      color: "white",
+      backgroundColor: "red",
+      padding: { x: 20, y: 10 },
+    })
+    .setOrigin(0.5, 0.5)
+    .setInteractive()
+    .setVisible(false); // Hide it initially
+
+  // Make button clickable
+
+  restartButton.on("pointerdown", () => {
+    restart();
+    restartButton.setVisible(false); // Hide button after restart
+  });
+
+  // Make button follow camera
+  restartButton.setScrollFactor(0);
+  //   this.input.on('pointerdown', function() {
+  //     bird.setVelocityY(-200); // Same upward velocity as spacebar
+  // });
 
   // messageToPlayer = this.add.text(0, 0, "Press 'Enter' to start", {
   //   fontFamily: "'Times New Roman', Times, serif",
@@ -106,7 +132,7 @@ function create() {
     .setOrigin(0.5, 0.5);
 
   messageToPlayer = this.add
-    .text(450, 530, "Press 'Space Bar' to start", {
+    .text(450, 530, "Press 'Enter' to start", {
       fontFamily: "Georgia, 'Times New Roman', Times, serif",
       fontSize: "20px",
       color: "black",
@@ -167,30 +193,33 @@ function update() {
   }
 
   if (isGameStarted == true) {
-    messageToPlayer.text = "Press the 'Space bar' to fly Level Easy";
+    messageToPlayer.text =
+      "To fly Press the 'Space bar' or Click on the screen";
   }
 
   if (bird.x >= 4000) {
     levelIndicator.text = "Level: Middle";
-    levelIndicator.setColor("blue")
+    levelIndicator.setColor("blue");
     bird.body.velocity.x = 100;
   }
 
   if (bird.x >= 10000) {
     levelIndicator.text = "Level: Hard";
-    levelIndicator.setColor("red")
+    levelIndicator.setColor("red");
     bird.body.velocity.x = 110;
   }
-    if (bird.x >= 15000) {
+  if (bird.x >= 15000) {
     levelIndicator.text = "Level: Insane";
-    levelIndicator.setColor("purple")
+    levelIndicator.setColor("purple");
     bird.body.velocity.x = 120;
   }
 
   if (hasLanded == true || hitColumn == true) {
-    bird.body.velocity.x = 0;
-    messageToPlayer.text = "You acrashed!  Press 'r' to restart";
+    messageToPlayer.text =
+      "You crashed! To restart Press 'r' or Click on 'Restart' button";
+    restartButton.setVisible(true);
     if (cursors.r && cursors.r.isDown) {
+      restartButton.setVisible(false); // Hide it initially
       restart();
     }
   }
@@ -203,6 +232,9 @@ function update() {
     if (bird.x >= 200 * i) {
       distanceCounter.text = i;
     }
+  }
+  if (cursors.pointer.isDown) {
+    bird.setVelocityY(-200);
   }
 }
 
@@ -217,5 +249,32 @@ function restart() {
   hitColumn = false;
   isGameStarted = false;
   messageToPlayer.text = "Press 'Enter' to start";
+  levelIndicator.text = "Level: Easy";
+  levelIndicator.setColor("green");
   distanceCounter.text = "0";
+
+  if (bird.x >= 4000) {
+    levelIndicator.text = "Level: Middle";
+    levelIndicator.setColor("blue");
+    bird.body.velocity.x = 100;
+  }
+
+  if (bird.x >= 10000) {
+    levelIndicator.text = "Level: Hard";
+    levelIndicator.setColor("red");
+    bird.body.velocity.x = 110;
+  }
+  if (bird.x >= 15000) {
+    levelIndicator.text = "Level: Insane";
+    levelIndicator.setColor("purple");
+    bird.body.velocity.x = 120;
+  }
+
+  if (bird.x >= 20020) {
+    messageToPlayer.text = "Congrats! You won!";
+  }
+
+  if (hasLanded == true || hitColumn == true) {
+    messageToPlayer.text = "You crashed! To restart Press 'r' or Click on 'Restart' button";
+  }
 }
